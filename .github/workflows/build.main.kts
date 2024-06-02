9370200f8ea166ad7b5a4e7b1efdfef0aefa8aca
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-06-02 13:13:28 +0200

    Add build workflow (#2)

---
+++ .github/workflows/build.main.kts
@@ -0,0 +1,33 @@
+#!/usr/bin/env kotlin
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.15.0")
+
+@file:Repository("https://github-workflows-kt-bindings.colman.com.br/binding/")
+@file:DependsOn("actions:checkout:v4")
+@file:DependsOn("gradle:actions__setup-gradle:v3")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.writeToFile
+
+workflow(
+    name = "Build",
+    on = listOf(
+        Push(branches = listOf("main")),
+        PullRequest(),
+    ),
+    sourceFile = __FILE__.toPath(),
+) {
+    job(
+        id = "build",
+        runsOn = RunnerType.UbuntuLatest,
+    ) {
+        uses(action = Checkout())
+        uses(action = ActionsSetupGradle())
+        run(command = "./gradlew build")
+    }
+}.writeToFile()
