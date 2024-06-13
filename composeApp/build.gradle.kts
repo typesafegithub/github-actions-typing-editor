d6a8da11cac19b540a68aae1643735fc337630d9
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-06-13 11:58:39 +0200

    Update Kotlin to 2.0.0 and Compose (#7)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.jetbrainsCompose)
 }
 
