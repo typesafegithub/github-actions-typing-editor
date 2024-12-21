72b3d5ccdb20e12ab474e89aecee1971009e1e03
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-12-21 10:39:48 +0100

    Make the test pass

--- composeApp/src/commonTest/kotlin/ExampleTest.kt
+++ composeApp/src/commonTest/kotlin/ExampleTest.kt
@@ -28,7 +28,7 @@ class ExampleTest {
         }
 
         // Tests the declared UI with assertions and actions of the Compose Multiplatform testing API
-        onNodeWithTag("text").assertTextEquals("Helloooo")
+        onNodeWithTag("text").assertTextEquals("Hello")
         onNodeWithTag("button").performClick()
         onNodeWithTag("text").assertTextEquals("Compose")
     }
